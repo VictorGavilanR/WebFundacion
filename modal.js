f09ba9217eec8d6806a94f5d0cf3162a1a8d5b79
Luis Matias Melo Guzman <luismatiasmg07@gmail.com>
2024-09-09 21:31:34 -0300

    Cambiar luego los nombres de las cosas ya que le puse pruebas+ agregue la seccion aparte de equipo con el modal

---
+++ modal.js
@@ -0,0 +1,18 @@
+function openModal(imgSrc, title, description) {
+    document.getElementById('modal-img').src = imgSrc;
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-description').innerText = description;
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
