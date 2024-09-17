8318cd57b08f0aeb7de0a5a7c8680e507560b0bb
VictorGavilanR <victormanuelgavilanr18@gmail.com>
2024-09-17 02:18:19 -0300

    Commit
    Agrege tus cosas a una carpeta para que no sea vea desordenado. Creer otra portada la cual nos quedaremos con esa si te gusta. falta hacerla responsiva

--- script.js
+++ script.js
@@ -93,3 +93,21 @@ window.addEventListener("scroll", function() {
   lastScrollTop = scrollTop;
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", function() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(n => n.addEventListener("click", function() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
+});
